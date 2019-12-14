327e6f6e76cfb928de518058c32f6fb9a65ccf90
Maher Santina <maher.santina90@gmail.com>
2019-12-14 13:22:41 +1100

    Added user and vehicle

---
+++ Sources/DSWorkshop/Models/User.swift
@@ -0,0 +1,46 @@
+//
+//  Invoice.swift
+//  App
+//
+//  Created by Maher Santina on 4/28/19.
+//
+
+import Vapor
+import Fluent
+import FluentMySQL
+import DSCore
+
+public struct WorkshopUserRow {
+
+    public enum CodingKeys: String, CodingKey, CaseIterable {
+        case id
+        case email
+    }
+
+    public var id: Int?
+    public var email: String
+
+    public init(id: Int? = nil, email: String) {
+        self.id = id
+        self.email = email
+    }
+}
+
+extension WorkshopUserRow: DSModel {
+    public static func routePath() throws -> String {
+        return "workshop-user"
+    }
+
+    public static var entity: String = "WorkshopUser"
+}
+
+extension WorkshopUserRow: Hashable {
+
+    public static func == (lhs: WorkshopUserRow, rhs: WorkshopUserRow) -> Bool {
+        return lhs.id == rhs.id
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
---
+++ Sources/DSWorkshop/Models/Vehicle.swift
@@ -0,0 +1,49 @@
+//
+//  Invoice.swift
+//  App
+//
+//  Created by Maher Santina on 4/28/19.
+//
+
+import Vapor
+import Fluent
+import FluentMySQL
+import DSCore
+
+public struct VehicleRow {
+
+    public enum CodingKeys: String, CodingKey, CaseIterable {
+        case id
+        case name
+        case userID
+    }
+
+    public var id: Int?
+    public var name: String
+    public var userID: WorkshopUserRow.ID
+
+    public init(id: Int? = nil, name: String, userID: WorkshopUserRow.ID) {
+        self.id = id
+        self.name = name
+        self.userID = userID
+    }
+}
+
+extension VehicleRow: DSModel {
+    public static func routePath() throws -> String {
+        return "vehicle"
+    }
+
+    public static var entity: String = "Vehicle"
+}
+
+extension VehicleRow: Hashable {
+
+    public static func == (lhs: VehicleRow, rhs: VehicleRow) -> Bool {
+        return lhs.id == rhs.id
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
